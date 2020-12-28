6cbdd9fd54584e73cab1b2751827752130d189d8
ivanab <ivana@opticaltone.com>
2020-12-28 20:58:49 +0100

    Started project

---
+++ SistemZaRaspodeluZadataka/src/main/java/domenObject/Role.java
@@ -0,0 +1,5 @@
+package domenObject;
+
+public class Role {
+
+}
---
+++ SistemZaRaspodeluZadataka/src/main/java/domenObject/User.java
@@ -0,0 +1,13 @@
+package domenObject;
+
+import java.util.List;
+
+public class User {
+
+	private String email;
+	private String name;
+	private String password;
+	private List<Task> task;
+	private List<Role> roles;
+			
+}
