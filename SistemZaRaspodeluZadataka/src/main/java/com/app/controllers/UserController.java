15747bcb9c41d807f6b24f1a2f2c14e854ceda42
ivanab <ivana@opticaltone.com>
2020-12-30 21:46:24 +0100

    Search users

--- SistemZaRaspodeluZadataka/src/main/java/com/app/controllers/UserController.java
+++ SistemZaRaspodeluZadataka/src/main/java/com/app/controllers/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.app.services.UserService;
 
@@ -14,8 +15,8 @@ public class UserController {
 	private UserService userService;
 	
 	@GetMapping("/users")
-	public String listUsers(Model model) {
-		model.addAttribute("users", userService.findAll());
+	public String listUsers(Model model, @RequestParam(defaultValue="") String name) {
+		model.addAttribute("users", userService.findByName(name));
 		return "views/list";
 	}
 }
