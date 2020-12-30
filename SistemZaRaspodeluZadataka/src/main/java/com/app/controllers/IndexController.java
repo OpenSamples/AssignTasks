e702278a1febd96553687fb22db0bc2ae4fd5898
ivanab <ivana@opticaltone.com>
2020-12-30 01:12:09 +0100

    Small changes

--- SistemZaRaspodeluZadataka/src/main/java/com/app/controllers/IndexController.java
+++ SistemZaRaspodeluZadataka/src/main/java/com/app/controllers/IndexController.java
@@ -2,13 +2,12 @@ package com.app.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.ui.Model;
+
 @Controller
 public class IndexController {
 
-	@GetMapping({"/", "/index"})
-	public String showIndexPage(Model model) {
-		model.addAttribute("name", "John");
+	@GetMapping("/")
+	public String showIndexPage() {
 		return "index";
 	}
 }
